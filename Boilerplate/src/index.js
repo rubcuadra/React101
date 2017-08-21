b16832d9677105ceb4bad153c9b6153b5ad45a78
rubcuadra <rubcuadra@gmail.com>
2017-08-21 16:25:20 -0500

    :tada:

---
+++ Boilerplate/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+  	<BrowserRouter>
+  	  <div>
+  	  	<Switch>
+	  	  	<Route path='/' component={App}/>
+  	  	</Switch>
+  	  </div>
+  	</BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
