37581ee69628a1ce048048791ed2981d26b97103
rubcuadra <rubcuadra@gmail.com>
2017-08-25 20:16:23 -0500

    :tada: new project

---
+++ Test/test/components/app_test.js
@@ -0,0 +1,11 @@
+import {renderComponent, expect} from '../test_helper';
+import App from '../../src/components/app';
+
+//Agrupar tareas similares
+describe('App') //Solo sirve para el reporte
+
+//Probar atributos del component
+it('Shows correct text')
+
+//Hacer assertions
+expect
