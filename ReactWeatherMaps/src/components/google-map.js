092e4b8653e8bad68ac88658d65507a87ab0a060
rubcuadra <rubcuadra@gmail.com>
2017-08-25 19:10:07 -0500

    added stuff for map

--- ReactWeatherMaps/src/components/google-map.js
+++ ReactWeatherMaps/src/components/google-map.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
 
 export default class GoogleMap extends Component{
+	shouldComponentUpdate(){
+		return false; //Jamas cambiara este
+	}
+
+	// Si estan intentando re renderearlo con nuevas lat lng
+	componentWillReceiveProps(nextProps){
+		this.map.panTo(
+			{
+				lat:nextProps.lat, 
+				lng:nextProps.lon
+			});
+	}
+
 	componentDidMount(){
 		new google.maps.Map(this.refs.map, {
 			zoom: 12,
