a23613b9eb754ac52a51cd5ef09e47fc3169f505
rubcuadra <rubcuadra@gmail.com>
2017-08-30 20:56:59 -0500

    Starting new projec

---
+++ AuthExample/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/header';
+import App from './components/app';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+  	  <div>
+  	    <Header/>
+  	  	<Switch>
+	  	  	<Route path='/' component={ App }/>
+  	  	</Switch>
+  	  </div>
+  	</BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
