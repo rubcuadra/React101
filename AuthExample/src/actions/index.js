627d042bc26d657cc683922de7fb0772b56d8172
rubcuadra <rubcuadra@gmail.com>
2017-08-31 15:15:27 -0500

    Testing with History

--- AuthExample/src/actions/index.js
+++ AuthExample/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const ROOT_URL = 'http://localhost:3090';
 
+//Podriamos pasarle la history para hacer el push...
 export function signinUser({email, password }){
 	//Submit email/password to the server
 
@@ -13,6 +14,12 @@ export function signinUser({email, password }){
 		//Invocamos dispatch() con una action cuando queramos
 		//Pasar un dict con los params
 		//El endpoint recibe esos dos params
-		axios.post( `${ROOT_URL}/signin`,{email,password});
+		axios.post( `${ROOT_URL}/signin`,{email,password})
+		.then(response=>{
+			console.log(response);
+		}).catch(e=>{
+			//Show error
+			console.log(e);
+		});
 	}
 }
